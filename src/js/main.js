7b28653727413bf72454b12da023900abce1c89d
VYUZ <valdiss1978@gmail.com>
2022-08-03 01:41:58 +0700

    editing 1

--- src/js/main.js
+++ src/js/main.js
@@ -100,7 +100,7 @@ document.addEventListener("DOMContentLoaded", () => {
   new ItcSimpleSlider(".itcss", {
     loop: true,
     autoplay: true,
-    interval: 4000,
+    interval: 6000,
     swipe: true,
     indicators: true,
   });
@@ -132,20 +132,20 @@ wow.init();
 // new WOW().init();
 ///////////////////////////////////
 ///load more /////
-document.addEventListener("DOMContentLoaded", function () {
-  const limit = 5;
-  const nextOpen = (wrapper, button) => {
-    const boxs = wrapper.querySelectorAll(".portfolio__item"),
-      len = boxs.length - 2,
-      endBox = wrapper.querySelector(".nextstop"),
-      index = [...boxs].indexOf(endBox) + limit;
-    if (endBox) endBox.classList.remove("nextstop");
-    if (index < len) boxs[index].classList.add("nextstop");
-    else button.remove();
-  };
-  document.querySelectorAll(".portfolio__items").forEach((wrapper) => {
-    const button = wrapper.nextElementSibling;
-    button.addEventListener("click", nextOpen.bind(null, wrapper, button));
-    nextOpen(wrapper, button);
-  });
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   const limit = 5;
+//   const nextOpen = (wrapper, button) => {
+//     const boxs = wrapper.querySelectorAll(".portfolio__item"),
+//       len = boxs.length - 2,
+//       endBox = wrapper.querySelector(".nextstop"),
+//       index = [...boxs].indexOf(endBox) + limit;
+//     if (endBox) endBox.classList.remove("nextstop");
+//     if (index < len) boxs[index].classList.add("nextstop");
+//     else button.remove();
+//   };
+//   document.querySelectorAll(".portfolio__items").forEach((wrapper) => {
+//     const button = wrapper.nextElementSibling;
+//     button.addEventListener("click", nextOpen.bind(null, wrapper, button));
+//     nextOpen(wrapper, button);
+//   });
+// });
